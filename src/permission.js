b6553af8a0bdd8a91d9e17ee56deb46148152f67
niuyutian <niuyutian@csii.com.cn>
2022-09-20 09:25:55 +0800

    提交代码

--- src/permission.js
+++ src/permission.js
@@ -22,10 +22,15 @@ router.beforeEach(async(to,from,next)=>{
             // 如果不是登录页
             // 如果vuex有用的id,就不用获取,没有则获取
             if(!store.getters.userInfo.userId){
-                await store.dispatch('user/getUserInfo')
+                const { roles }=  await store.dispatch('user/getUserInfo')
                 // 这里是异步, 如果后续需要根据用户资料获取数据的话必须改成同步代码
+                // 筛选用户可用路由
+                const routes = await  store.dispatch('permission/fulterRouters',roles.menus)
+                router.addRoutes([...routes,{ path: '*', redirect: '/404', hidden: true }] )//添加动态路由到路由表
+                next(to.path)//相当于调到对应地址,相当于多做一次跳转
+            }else{
+                next()
             }
-            next()
         }
     }else {
         // 如果是白名单放行,否则就调到登录页
